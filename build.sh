ab7832fefcbf4888a46c3c1b482bcbb11a71f648
yeger <yeger00@gmail.com>
2019-10-06 15:42:04 -0400

    Adding build script and fixing formating

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+FLAGS=-DDEBUG
+g++-9 -c -std=c++2a $FLAGS program.cc -O3 -o program.o
+g++-9 -std=c++2a $FLAGS main.cc -O3 program.o -o main
+./main
+echo
+
+FLAGS=-DCREATE_MAP
+g++-9 -c -std=c++2a $FLAGS program.cc -O3 -o program.o
+g++-9 -std=c++2a $FLAGS crc_to_string.cc -O3 program.o -o crc_to_string
+./crc_to_string
+echo
+
+unset FLAGS
+g++-9 -c -std=c++2a $FLAGS program.cc -O3 -o program.o
+g++-9 -std=c++2a $FLAGS main.cc -O3 program.o -o main
+./main
+echo
--- crc_to_string.cc
+++ crc_to_string.cc
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <map>
-//#include "ctcrclog.hpp"
 
 extern std::map<uint32_t, std::string> crc_to_string;
 
 int main() {
 	std::map<uint32_t, std::string>::iterator it;
     
-        for (it=crc_to_string.begin(); it != crc_to_string.end(); it++)
+        for (it=crc_to_string.begin(); it != crc_to_string.end(); it++) {
                 std::cout << it->first << " : " << it->second << std::endl;
-        std::cout << '\n';
+	}
         return 0;
 }
